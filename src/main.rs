35579fff5c2464a9fe548bd58cca54f95c6409fe
James Carson <jms.crsn@gmail.com>
2015-04-15 23:07:49 -0400

    testing shit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let foo = 27;
+    let (x,y) = (1,2);
+    let mut bar : u8 = 0;
+    println!("foo: {}",foo);
+    println!("(x,y): ({},{})",x,y);
+    println!("bar: {}",bar);
+    bar = bar + 255;
+    println!("bar: {}",bar);
 }
