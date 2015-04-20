c2ad1345bce1b5348e16e91e2da87f64230af0f0
James Carson <jms.crsn@gmail.com>
2015-04-20 00:37:29 -0400

    add more shit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+struct Foo<'a> {
+    x: &'a i32,
+}
+
 fn main() {
     //comments
     let foo = 27;
@@ -25,10 +29,70 @@ fn main() {
     println!("first: {}",first);
     println!("second: {}",second);
     let z: fn(u8,u8) -> u8 = ret_sum;
+    foobar1(5);
+    foobar1(3);
+    let foo1: u8 = if 4 == 4 {8} else {10};
+    println!("foo1: {}",foo1);
+    for x in 0..10 {
+        println!("x: {}",x);
+    }
+    let mut done: bool = false;
+    let mut foo2: u8 = 0;
+    while !done {
+        println!("foo2: {}",foo2);
+        foo2 = foo2 + 1;
+        done = foo2 == 10;
+    }
+    let mut foo3: u8 = 0;
+    loop {
+        println!("foo3: {}",foo3);
+        foo3 = foo3 + 5;
+        if foo3 > 50 {break;}
+    }
+    for x in 0..10 {
+        if x%2 == 1 {continue;}
+        println!("x: {}",x);
+    }
+    let foo4 = Box::new(5);
+    let foo5 = add_one(foo4);
+    println!("foo4: {}",foo5);
+    let mut foo6: i32 = 10;
+    add_one_v2(&mut foo6);
+    println!("foo6: {}",foo6);
+    //---------------------------------------------------
+    let foo7 = &5;
+    let f = Foo { x: foo7 };
+    println!("f.x: {}",f.x);
+    // I think this SHOULD WORK, but it doesn't...
+    /*
+    {
+        let foo8;
+        let foo9 = &5;
+        let f1 = Foo { x: foo9 };
+        foo8 = &f1.x;
+    }
+    */
+}
+
+fn add_one_v2(x: &mut i32) {
+    *x+=1;
+}
+
+fn add_one(mut x: Box<i32>) -> Box<i32> {
+    *x += 1;
+    x
 }
 
 fn print_thing(x: u8) {
     println!("x: {}",x);
+}
+
+fn foobar1(x: u8) {
+    if x == 5 {
+        println!("You gave me 5!!");
+    } else {
+        println!("You didn't give me 5 :(");
+    }
 }
 
 /// Adds 2 Numbers
