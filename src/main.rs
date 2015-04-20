7c3201b6b5bdf7bce275684736fbbb327db7968d
James Carson <jms.crsn@gmail.com>
2015-04-19 20:47:51 -0400

    learned more shit

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,32 @@ fn main() {
     println!("bar: {}",bar);
     bar = bar + 255;
     println!("bar: {}",bar);
+    print_thing(45);
+    print_sum(24,20);
+    println!("sum2: {}",ret_sum(1,3));
+    let a = [0;10];
+    println!("a length: {}",a.len());
+    println!("a[5]: {}",a[5]);
+    let b = &a[2..4];
+    println!("b length: {}",b.len());
+    let tuple: (u8,&str) = (0,"apple");
+    let index: u8 = tuple.0;
+    let value: &str = tuple.1;
+    let (first,second) = tuple;
+    println!("index: {}",index);
+    println!("value: {}",value);
+    println!("first: {}",first);
+    println!("second: {}",second);
+}
+
+fn print_thing(x: u8) {
+    println!("x: {}",x);
+}
+
+fn print_sum(x: u8,y: u8) {
+    println!("sum: {}",x+y);
+}
+
+fn ret_sum(x: u8, y: u8) -> u8 {
+    x + y
 }
