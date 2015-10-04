dcc3418d2c146b229f61cee7fa503e46edeef951
James Carson <jms.crsn@gmail.com>
2015-10-03 23:09:03 -0400

    added while let

--- src/if_let_tests/mod.rs
+++ src/if_let_tests/mod.rs
@@ -1,6 +1,13 @@
 pub fn execute () {
+
     let number: Option<u8> = Some(4);
     if let Some(x) = number {
         assert_eq!(x,4);
     }
+
+    let mut vec = vec![1,3,2,4,3,5,4,6,5,7];
+    while let Some(x) = vec.pop() {
+        print!("{} ",x);
+    }
+    println!("");
 }
