921289affc82e206a588c65e04aefaf0e464dca7
James Carson <jms.crsn@gmail.com>
2015-10-13 22:07:55 -0400

    does it work with ncurses?

--- src/ffi_tests/mod.rs
+++ src/ffi_tests/mod.rs
@@ -5,6 +5,11 @@ extern {
     fn snappy_max_compressed_length(source_length: size_t) -> size_t;
 }
 
+#[link(name="ncurses")]
+extern {
+    fn has_mouse () -> bool;
+}
+
 pub fn execute () {
     let x = unsafe { snappy_max_compressed_length(100) };
     println!("x: {}",x);
