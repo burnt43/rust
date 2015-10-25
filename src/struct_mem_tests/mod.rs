152339de40a9746d3270b50f5598f0a7905d5daa
James Carson <jms.crsn@gmail.com>
2015-10-25 13:21:04 -0400

    more C vs Rust struct comparison

--- src/struct_mem_tests/mod.rs
+++ src/struct_mem_tests/mod.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 struct Foobar {
     x: u8,
     y: u8,
@@ -20,9 +22,21 @@ impl Foobar {
 
 pub fn execute () {
     let f = Foobar::new();
+    println!("{}",mem::size_of::<Foobar>());
     println!("{:p}",&f.x);
     println!("{:p}",&f.y);
     println!("{:p}",&f.z);
     println!("{:p}",&f.a);
     println!("{:p}",&f.b);
+    let raw_ptr: *const u8 = &f.x;
+    unsafe {
+        assert_eq!( *raw_ptr.offset(0), f.x );
+        assert_eq!( *raw_ptr.offset(1), f.y );
+        assert_eq!( *(raw_ptr.offset(4) as *const u32), f.z );
+        assert_eq!( *(raw_ptr.offset(8) as *const u32), f.a );
+        assert_eq!( *(raw_ptr.offset(16) as *const u64), f.b );
+        for i in 0..mem::size_of::<Foobar>() {
+            println!("{:2}: {:X}",i,*raw_ptr.offset(i as isize));
+        }
+    }
 }
