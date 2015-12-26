3349c9e51c15b588d5cb44280aa1e5314229e8ce
James Carson <jms.crsn@gmail.com>
2015-12-26 02:12:30 -0500

    make a macro to call the as_u64() method on all elements

--- src/heterogeneous_vector_tests/mod.rs
+++ src/heterogeneous_vector_tests/mod.rs
@@ -1,3 +1,13 @@
+macro_rules! numerize_animal_vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec : Vec<u64> = Vec::new();
+            $( temp_vec.push($x.as_u64()); )*
+            temp_vec
+        }
+    };
+}
+
 struct Cat {
     meow: u64,
 }
@@ -6,23 +16,36 @@ struct Dog {
     woof: u64,
 }
 
+impl Cat {
+    fn new (x: u64) -> Cat {
+        Cat { meow: x }
+    }
+}
+
+impl Dog {
+    fn new (x: u64) -> Dog {
+        Dog { woof: x }
+    }
+}
+
 trait NumerizeAnimal {
     fn as_u64 (&self) -> u64;
 }
 
 impl NumerizeAnimal for Cat {
     fn as_u64 (&self) -> u64 {
-        100
+        self.meow
     }
 }
 
 impl NumerizeAnimal for Dog {
     fn as_u64 (&self) -> u64 {
-        200
+        self.woof
     }
 }
 
 #[test]
 fn test1 () {
-    //let v : Vec<NumerizeAnimal> = Vec::new(); does not work. no Sized trait
+    let v : Vec<u64> = numerize_animal_vec![Cat::new(1),Dog::new(2)];
+    assert_eq!( *(v.last().unwrap()), 2 );
 }
