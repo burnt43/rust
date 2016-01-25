b87967be4f4c380f85c55bbb316b142b83b3209e
James Carson <jms.crsn@gmail.com>
2016-01-25 00:25:21 -0500

    i do not know how to return the generic stuff even though the compiler lets me write the syntax

--- src/jan_24_2016_tests/mod.rs
+++ src/jan_24_2016_tests/mod.rs
@@ -1,3 +1,27 @@
+enum Foobar {
+    Alpha(String),
+    Beta(u8),
+    Gamma(char),
+}
+
+trait Better {
+    fn better_func();
+}
+
+impl Better for String {
+    fn better_func() {
+        println!("String");
+    }
+}
+
+impl Foobar {
+    fn get<T>(&self) -> Result<T,()> where T: Better {
+        Err(())
+    }
+}
+
 #[test]
 fn sanity_check() {
+    let _x: Foobar = Foobar::Alpha(String::new());
+    let _result    = _x.get::<String>();
 }
