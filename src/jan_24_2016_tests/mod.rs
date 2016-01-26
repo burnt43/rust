0dbee7025590e4ee75f85e2773856a251b126a54
James Carson <jms.crsn@gmail.com>
2016-01-25 21:28:20 -0500

    thansk to reddit. there is a way to do the ::<>

--- src/jan_24_2016_tests/mod.rs
+++ src/jan_24_2016_tests/mod.rs
@@ -1,27 +1,37 @@
+// Thanks to /u/diwic
+use std::str::FromStr;
+
 enum Foobar {
     Alpha(String),
     Beta(u8),
-    Gamma(char),
 }
 
-trait Better {
-    fn better_func();
+trait Getter {
+    fn get_that_stuff(&Foobar) -> Result<Self,()>;
 }
 
-impl Better for String {
-    fn better_func() {
-        println!("String");
+impl Getter for u8 {
+    fn get_that_stuff(f: &Foobar) -> Result<u8,()> {
+        if let &Foobar::Beta(ref z) = f { Ok(*z) } else { Err(()) }
     }
 }
 
 impl Foobar {
-    fn get<T>(&self) -> Result<T,()> where T: Better {
-        Err(())
+    fn get<T>(&self) -> Result<T,()> where T: Getter {
+        T::get_that_stuff(self)
     }
 }
 
 #[test]
 fn sanity_check() {
-    let _x: Foobar = Foobar::Alpha(String::new());
-    let _result    = _x.get::<String>();
+    let _whatever: Foobar = Foobar::Beta(10u8);
+    assert!(_whatever.get::<u8>().is_ok());
+    let _other: Foobar = Foobar::Alpha(String::new());
+    assert!(_other.get::<u8>().is_err());
+}
+
+#[test]
+fn other_test() {
+    let _result = u8::from_str("8");
+    let _result = "8".parse::<u8>();
 }
