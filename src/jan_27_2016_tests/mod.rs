9f6e712792043a339e14358ae1baecb353aeee82
James Carson <jms.crsn@gmail.com>
2016-01-27 22:15:24 -0500

    i forgot how try worked for a second there

--- src/jan_27_2016_tests/mod.rs
+++ src/jan_27_2016_tests/mod.rs
@@ -30,7 +30,18 @@ impl JamesEncodable for u8 {
 }
 
 #[test]
-fn whatever() {
+fn still_trying_wacky_stuff() {
     assert!( u8::from_strinteger_result( &Strinteger::A(1u8) ).is_ok() );
     assert!( u8::from_strinteger_result( &Strinteger::B(String::new()) ).is_err() );
+}
+
+fn simple_try_stuff() {
+    fn foo(n: u8) -> Result<u8,()> {
+        if n == 10 { Ok(n) } else { Err(()) }
+    }
+    fn bar() -> Result<u8,()> {
+        let x: u8 = try!(foo(10));
+        Ok(x)
+    }
+    assert!(bar().is_ok());
 }
