a6369734fdca52c23dbe71c1028a0525b23cc9a4
James Carson <jms.crsn@gmail.com>
2016-02-04 23:55:25 -0500

    some stuff i was messing around with at the airport

--- src/jan_28_2016_tests/mod.rs
+++ src/jan_28_2016_tests/mod.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 enum Color {
     Red,
     Green,
@@ -17,4 +18,71 @@ fn default_values() {
     let _foo: String = Default::default();
     assert!(_foo.is_empty());
     let _foo: Color = Color::default();
+    assert_eq!(_foo, Color::Red);
+}
+
+struct Foo<'a> {
+    id: u8,
+    bars: Vec<&'a u8>,
+}
+
+impl<'a> Default for Foo<'a> {
+    fn default() -> Foo<'a> {
+        Foo {
+            id: 0,
+            bars: Vec::new(),
+        }
+    }
+}
+
+impl<'a> Foo<'a> {
+    fn new() -> Foo<'a> {
+        Self::default()
+    }
+    fn add_bar(&mut self, bar: &'a u8) {
+        self.bars.push(bar);
+    }
+}
+
+#[test]
+fn messing_around_with_lifetimes() {
+    let      _a:  u8 = 1;
+    let mut foo: Foo = Foo::default();
+    foo.add_bar(&_a);
+}
+
+struct Person {
+    name: String,
+}
+
+impl Default for Person {
+    fn default() -> Person {
+        Person { name: "Rick".to_string() }
+    }
+}
+
+struct Judger;
+impl Judger {
+    fn bad_judge_person(person: Person) -> u8 {
+        0
+    }
+    fn good_judge_person(person: &Person) -> u8 {
+        0
+    }
+}
+
+#[test]
+fn borrowing() {
+    let person: Person = Person::default();
+    let _ = Judger::bad_judge_person(person);
+    //assert_eq!(&person.name, "Rick"); person no longer owns the data
+    let person: Person = Person::default();
+    let _ = Judger::good_judge_person(&person);
+    assert_eq!(&person.name, "Rick"); //this is ok because i sent a reference to the Judger
+
+    let mut x: Person = Person::default();
+    let     y: Person = x;
+    x = Person{ name: "Joe".to_string() };
+    assert_eq!(&x.name,"Joe");
+    assert_eq!(&y.name,"Rick");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+/*
 extern crate testing;
 extern crate libc;
 extern crate ncurses;
 use ncurses::Window;
+*/
 
 fn main() {
+    /*
     let window: Window = ncurses::initscr();
     window.printw("TEST");
     window.getch();
     ncurses::endwin();
+    */
 }
