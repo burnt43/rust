89d68a05f5145c7d9120ddf27c647284dc033e57
James Carson <jms.crsn@gmail.com>
2016-02-05 00:27:51 -0500

    more stuff

--- src/feb_04_2016_tests/mod.rs
+++ src/feb_04_2016_tests/mod.rs
@@ -14,8 +14,12 @@ fn foo () {
     let rear_left   = Wheel { size: 18, name: "OZ Racing".to_string() };
     let rear_right  = Wheel { size: 18, name: "OZ Racing".to_string() };
 
-    let big_wheel  = Wheel { size: 20, name: "Enkei".to_string() };
-
     let _car = Car { wheels: [&front_left, &front_right, &rear_left, &rear_right] };
-    let _car = Car { wheels: [&big_wheel, &big_wheel, &big_wheel, &big_wheel] };
+
+    let mut big_wheel  = Wheel { size: 20, name: "Enkei".to_string() };
+    let mut _car = Car { wheels: [&big_wheel, &big_wheel, &big_wheel, &big_wheel] };
+    // cannot be done?
+    // let foo: &mut Wheel = &mut big_wheel;
+    // let _tmp: &mut Wheel = _car.wheels[0].as_mut();
+    // _tmp.size = 21;
 }
